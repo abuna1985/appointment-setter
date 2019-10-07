ed1f734ff40c35b3938c3346ac374b61c8a0b815
abuna1985 <adamjabundis@gmail.com>
2019-10-07 12:10:27 -0700

    Add header for the app with bootstrap styling.

---
+++ src/components/AppointmentHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="navbarHeader">
+    <div class="navbar navbar-dark bg-primary box-shadow">
+      <div class="container-fluid">
+        <a href="#" class="navbar-brand d-flex align-items-center">
+          <font-awesome-icon icon="tooth" class="mr-2"/>
+          <strong>ACME Children's Dental Center</strong>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'Header'
+  }
+</script>
